98ebf0d777f911a0b31c15030cfb05d93ca1d635
David Taylor <david@taylorhq.com>
2021-03-25 17:59:15 +0000

    FIX: When tags are not specified, enable everywhere
    When splitting an empty string, js will return an empty string. Therefore we need to filter out those empty values before calling `.length`

--- javascripts/discourse/initializers/init-topic-excerpts.js
+++ javascripts/discourse/initializers/init-topic-excerpts.js
@@ -7,7 +7,7 @@ const enabledCategories = settings.enabled_categories
   .map((id) => parseInt(id, 10))
   .filter((id) => id);
 
-const enabledTags = settings.enabled_tags.split("|");
+const enabledTags = settings.enabled_tags.split("|").filter((tag) => tag);
 
 export default {
   name: "topic-excerpts-init",
