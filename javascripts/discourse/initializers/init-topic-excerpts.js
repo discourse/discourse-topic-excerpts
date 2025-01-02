be30e4e70e3bebeb917f5d6d9fa86adeca75c11f
Isaac Janzen <isaac.janzen@discourse.org>
2025-01-02 13:50:23 -0600

    DEV: Add back old implementation

--- javascripts/discourse/initializers/init-topic-excerpts.js
+++ javascripts/discourse/initializers/init-topic-excerpts.js
@@ -1,4 +1,8 @@
+import { getOwner } from "@ember/application";
+import { service } from "@ember/service";
 import { withPluginApi } from "discourse/lib/plugin-api";
+import { withSilencedDeprecations } from "discourse-common/lib/deprecated";
+import discourseComputed from "discourse-common/utils/decorators";
 
 const enabledCategories = settings.enabled_categories
   .split("|")
@@ -43,6 +47,60 @@ export default {
           return value; // Return default value
         }
       );
+
+      // TODO: cvx - remove after the glimmer topic list transition
+      withSilencedDeprecations("discourse.hbr-topic-list-overrides", () => {
+        api.modifyClass("component:topic-list-item", {
+          pluginId: "discourse-topic-excerpts",
+
+          excerptsRouter: service("router"),
+
+          @discourseComputed(
+            "excerptsRouter.currentRouteName",
+            "excerptsRouter.currentRoute.attributes.category.id"
+          )
+          excerptsViewingCategoryId(currentRouteName, categoryId) {
+            if (!currentRouteName.match(/^discovery\./)) {
+              return;
+            }
+            return categoryId;
+          },
+
+          @discourseComputed(
+            "excerptsRouter.currentRouteName",
+            "excerptsRouter.currentRoute.attributes.id", // For discourse instances earlier than https://github.com/discourse/discourse/commit/f7b5ff39cf
+            "excerptsRouter.currentRoute.attributes.tag.id"
+          )
+          excerptsViewingTag(currentRouteName, legacyTagId, tagId) {
+            if (!currentRouteName.match(/^tag\.show/)) {
+              return;
+            }
+            return tagId || legacyTagId;
+          },
+
+          @discourseComputed("excerptsViewingCategoryId", "excerptsViewingTag")
+          expandPinned(viewingCategory, viewingTag) {
+            const overrideEverywhere =
+              enabledCategories.length === 0 && enabledTags.length === 0;
+
+            const overrideInCategory =
+              enabledCategories.includes(viewingCategory);
+            const overrideInTag = enabledTags.includes(viewingTag);
+
+            const overrideOnDevice = getOwner(this).lookup("service:site")
+              .mobileView
+              ? settings.show_excerpts_mobile
+              : settings.show_excerpts_desktop;
+
+            return (overrideEverywhere ||
+              overrideInTag ||
+              overrideInCategory) &&
+              overrideOnDevice
+              ? true
+              : this._super();
+          },
+        });
+      });
     });
   },
 };
