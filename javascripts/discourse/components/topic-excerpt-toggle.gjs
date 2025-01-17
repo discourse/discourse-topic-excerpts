e8c49052d398ae49ee32f69d3e57d69ceeea2e88
awesomerobot <kris.aubuchon@discourse.org>
2025-01-17 17:14:21 -0500

    FEATURE: add optional excerpt toggle

---
+++ javascripts/discourse/components/topic-excerpt-toggle.gjs
@@ -0,0 +1,55 @@
+import Component from "@glimmer/component";
+import { on } from "@ember/modifier";
+import { action } from "@ember/object";
+import { service } from "@ember/service";
+import icon from "discourse-common/helpers/d-icon";
+import { i18n } from "discourse-i18n";
+
+export default class ExcerptToggle extends Component {
+  @service excerptState;
+  @service site;
+  @service router;
+
+  constructor() {
+    super(...arguments);
+    this.excerptState.excerptClass(this.excerptState.prefersExcerpt);
+  }
+
+  get shouldShow() {
+    const categoriesRoute =
+      this.router.currentRouteName === "discovery.categories";
+
+    if (!settings.show_toggle || categoriesRoute) {
+      return;
+    }
+
+    const isTopicHeader =
+      this.args.outletArgs?.name === "default" ||
+      this.args.outletArgs?.name === "topic.title" ||
+      this.site.mobileView;
+
+    return (
+      this.excerptState.shouldExpandPinned() &&
+      isTopicHeader &&
+      !this.args.outletArgs.bulkSelectEnabled
+    );
+  }
+
+  get buttonIcon() {
+    return this.excerptState.prefersExcerpt ? "square-check" : "far-square";
+  }
+
+  @action
+  toggleExcerpt() {
+    this.excerptState.toggleExcerpt();
+  }
+
+  <template>
+    {{#if this.shouldShow}}
+      <button {{on "click" this.toggleExcerpt}} class="excerpt-toggle">
+        {{icon this.buttonIcon}}
+        <span>{{i18n (themePrefix "show_excerpts")}}</span>
+      </button>
+    {{/if}}
+  </template>
+}
--- javascripts/discourse/initializers/init-topic-excerpts.js
+++ javascripts/discourse/initializers/init-topic-excerpts.js
@@ -1,41 +1,17 @@
-import { getOwner } from "@ember/application";
-import { service } from "@ember/service";
 import { withPluginApi } from "discourse/lib/plugin-api";
 import { withSilencedDeprecations } from "discourse-common/lib/deprecated";
-import discourseComputed from "discourse-common/utils/decorators";
-
-const enabledCategories = settings.enabled_categories
-  .split("|")
-  .map((id) => parseInt(id, 10))
-  .filter((id) => id);
-const enabledTags = settings.enabled_tags.split("|").filter((tag) => tag);
 
 export default {
   name: "topic-excerpts-init",
 
   initialize() {
     withPluginApi("1.34.0", (api) => {
-      const discovery = api.container.lookup("service:discovery");
+      const excerptState = api.container.lookup("service:excerpt-state");
+
       api.registerValueTransformer(
         "topic-list-item-expand-pinned",
-        ({ value, context }) => {
-          const overrideEverywhere =
-            enabledCategories.length === 0 && enabledTags.length === 0;
-          const overrideInCategory = enabledCategories.includes(
-            discovery.category?.id
-          );
-          const overrideInTag = enabledTags.includes(discovery.tag?.id);
-          const overrideOnDevice = context.mobileView
-            ? settings.show_excerpts_mobile
-            : settings.show_excerpts_desktop;
-
-          if (
-            (overrideEverywhere || overrideInTag || overrideInCategory) &&
-            overrideOnDevice
-          ) {
-            return true;
-          }
-          return value; // Return default value
+        ({ value }) => {
+          return excerptState.shouldExpandPinned() || value;
         }
       );
 
@@ -44,51 +20,9 @@ export default {
         api.modifyClass("component:topic-list-item", {
           pluginId: "discourse-topic-excerpts",
 
-          excerptsRouter: service("router"),
-
-          @discourseComputed(
-            "excerptsRouter.currentRouteName",
-            "excerptsRouter.currentRoute.attributes.category.id"
-          )
-          excerptsViewingCategoryId(currentRouteName, categoryId) {
-            if (!currentRouteName.match(/^discovery\./)) {
-              return;
-            }
-            return categoryId;
-          },
-
-          @discourseComputed(
-            "excerptsRouter.currentRouteName",
-            "excerptsRouter.currentRoute.attributes.id", // For discourse instances earlier than https://github.com/discourse/discourse/commit/f7b5ff39cf
-            "excerptsRouter.currentRoute.attributes.tag.id"
-          )
-          excerptsViewingTag(currentRouteName, legacyTagId, tagId) {
-            if (!currentRouteName.match(/^tag\.show/)) {
-              return;
-            }
-            return tagId || legacyTagId;
-          },
-
-          @discourseComputed("excerptsViewingCategoryId", "excerptsViewingTag")
-          expandPinned(viewingCategory, viewingTag) {
-            const overrideEverywhere =
-              enabledCategories.length === 0 && enabledTags.length === 0;
-
-            const overrideInCategory =
-              enabledCategories.includes(viewingCategory);
-            const overrideInTag = enabledTags.includes(viewingTag);
-
-            const overrideOnDevice = getOwner(this).lookup("service:site")
-              .mobileView
-              ? settings.show_excerpts_mobile
-              : settings.show_excerpts_desktop;
-
-            return (overrideEverywhere ||
-              overrideInTag ||
-              overrideInCategory) &&
-              overrideOnDevice
-              ? true
-              : this._super();
+          expandPinned() {
+            const shouldExpand = excerptState.shouldExpandPinned();
+            return shouldExpand || this._super(...arguments);
           },
         });
       });
